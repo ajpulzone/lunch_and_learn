4ff8ea14922ea020d3a9744fe876f6bf0b032214
Annie Pulzone <ajpulzone@gmail.com>
2023-01-17 20:03:40 -0800

    Refactored testing for favorites to include sad paths and implemented vcr

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,18 +2,24 @@ class Api::V1::FavoritesController < ApplicationController
 
   def create
     user = User.where(user_api_key: favorite_params[:user_api_key]).first 
-    user_favorite = user.favorites.new(favorite_params)
-    if user_favorite.save
+    if user == nil
+      render json: { "errors": "User could not be found, favorite not added" }, status: 400
+    elsif
+      user_favorite = user.favorites.new(favorite_params)
+      user_favorite.save
       render json: {"success": "Favorite added successfully" }, status: 201
-    else
-      render json: { "errors": "Favorite was not added" }, status: 400
     end
   end
 
   def index
     user = User.where(user_api_key: params[:user_api_key]).first
-    render json: FavoriteSerializer.new(user.favorites), status: 201
+    if user == nil
+      render json: { "errors": "User could not be found" }, status: 400
+    elsif
+      render json: FavoriteSerializer.new(user.favorites), status: 201
+    end
   end
+
 private
   def favorite_params
     params.require(:favorite).permit(:user_api_key, :country, :recipe_link, :recipe_title)
--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -2,72 +2,110 @@ require "rails_helper"
 
 RSpec.describe "Favorites API" do
   describe "Favorite Creation" do
-    describe "happy path" do
-      before(:each) do
-        body = { "name": "Drake", "email": "imamuppet@gmail.com" }
-        headers = { "Content-Type": "application/json", Accept: "application/json" }
-        post "/api/v1/users", headers: headers, params: JSON.generate(user: body)
-        @user = User.last
-      end
+    before(:each) do
+      body = { "name": "Drake", "email": "imamuppet@gmail.com" }
+      headers = { "Content-Type": "application/json", Accept: "application/json" }
+      post "/api/v1/users", headers: headers, params: JSON.generate(user: body)
+      @user = User.last
+    end
 
-      it "creates a favorite recipe for a user" do
-        
-        body = { "user_api_key": "#{@user.user_api_key}", "country": "Ireland", "recipe_link": "http://www.edamam.com/recipe/cheddar-colcannon-torte-97c47f3162d83a9da30673929bbf8f7a/ireland", "recipe_title": "Cheddar Colcannon Torte"}
+    it "happy path: it creates a favorite recipe for a user" do
+      body = { "user_api_key": "#{@user.user_api_key}", "country": "Ireland", "recipe_link": "http://www.edamam.com/recipe/cheddar-colcannon-torte-97c47f3162d83a9da30673929bbf8f7a/ireland", "recipe_title": "Cheddar Colcannon Torte"}
+      headers = { "Content-Type": "application/json", Accept: "application/json" }
+      post "/api/v1/favorites", headers: headers, params: JSON.generate(favorite: body)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to be_a(Hash)
+      expect(parsed_response).to have_key(:success)
+      expect(parsed_response[:success]).to eq("Favorite added successfully")
+    end
+
+    it "sad path: it returns a 404 error and message if a users user_api_key does not match the favorite 
+      user_api_key, and a favorite is not created" do
+        body = { "user_api_key": "123456abcdefg", "country": "Ireland", "recipe_link": "http://www.edamam.com/recipe/cheddar-colcannon-torte-97c47f3162d83a9da30673929bbf8f7a/ireland", "recipe_title": "Cheddar Colcannon Torte"}
         headers = { "Content-Type": "application/json", Accept: "application/json" }
         post "/api/v1/favorites", headers: headers, params: JSON.generate(favorite: body)
 
-        expect(response).to be_successful
-        expect(response.status).to eq(201)
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
 
         parsed_response = JSON.parse(response.body, symbolize_names: true)
-
+        
         expect(parsed_response).to be_a(Hash)
-        expect(parsed_response).to have_key(:success)
-        expect(parsed_response[:success]).to eq("Favorite added successfully")
-      end
+        expect(parsed_response).to have_key(:errors)
+        expect(parsed_response[:errors]).to eq("User could not be found, favorite not added")
     end
   end
 
   describe "Get a Users Favorites" do
-    describe "happy path" do 
-      before(:each) do
-        body = { "name": "Drake", "email": "imamuppet@gmail.com" }
-        headers = { "Content-Type": "application/json", Accept: "application/json" }
-        post "/api/v1/users", headers: headers, params: JSON.generate(user: body)
-        @user = User.last
+    before(:each) do
+      body = { "name": "Drake", "email": "imamuppet@gmail.com" }
+      headers = { "Content-Type": "application/json", Accept: "application/json" }
+      post "/api/v1/users", headers: headers, params: JSON.generate(user: body)
+      @user = User.last
 
-        body1 = { "user_api_key": "#{@user.user_api_key}", "country": "Ireland", "recipe_link": "http://www.edamam.com/recipe/cheddar-colcannon-torte-97c47f3162d83a9da30673929bbf8f7a/ireland", "recipe_title": "Cheddar Colcannon Torte"}
-        headers1 = { "Content-Type": "application/json", Accept: "application/json" }
-        post "/api/v1/favorites", headers: headers1, params: JSON.generate(favorite: body1)
-      end
-      
-      it "returns json of a specified users favorites" do
-
-        get "/api/v1/favorites?user_api_key=#{@user.user_api_key}"
-
-        expect(response).to be_successful
-        expect(response.status).to eq(201)
-
-        response_body = JSON.parse(response.body, symbolize_names: true)
-
-        user_favorites = response_body[:data]
-
-        expect(user_favorites.count).to eq(1)
-        expect(user_favorites).to be_an(Array)
-        expect(user_favorites[0]).to have_key(:id)
-        expect(user_favorites[0]).to have_key(:type)
-        expect(user_favorites[0]).to have_key(:attributes)
-        expect(user_favorites[0][:attributes]).to have_key(:country)
-        expect(user_favorites[0][:attributes]).to have_key(:recipe_link)
-        expect(user_favorites[0][:attributes]).to have_key(:recipe_title)
-        expect(user_favorites[0][:attributes]).to have_key(:created_at)
-        expect(user_favorites[0][:attributes]).to_not have_key(:user_api_key)
-
-        expect(user_favorites[0][:type]).to eq("favorite")
-        expect(user_favorites[0][:attributes][:country]).to eq("Ireland")
-        expect(user_favorites[0][:attributes][:recipe_link]).to eq("http://www.edamam.com/recipe/cheddar-colcannon-torte-97c47f3162d83a9da30673929bbf8f7a/ireland")
-        expect(user_favorites[0][:attributes][:recipe_title]).to eq("Cheddar Colcannon Torte")        
-      end    
+      body1 = { "user_api_key": "#{@user.user_api_key}", "country": "Ireland", "recipe_link": "http://www.edamam.com/recipe/cheddar-colcannon-torte-97c47f3162d83a9da30673929bbf8f7a/ireland", "recipe_title": "Cheddar Colcannon Torte"}
+      headers1 = { "Content-Type": "application/json", Accept: "application/json" }
+      post "/api/v1/favorites", headers: headers1, params: JSON.generate(favorite: body1)
     end
+    
+    it "happy path: returns json of a specified users favorites if a user is found" do
+
+      get "/api/v1/favorites?user_api_key=#{@user.user_api_key}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      user_favorites = response_body[:data]
+
+      expect(user_favorites.count).to eq(1)
+      expect(user_favorites).to be_an(Array)
+      expect(user_favorites[0]).to have_key(:id)
+      expect(user_favorites[0]).to have_key(:type)
+      expect(user_favorites[0]).to have_key(:attributes)
+      expect(user_favorites[0][:attributes]).to have_key(:country)
+      expect(user_favorites[0][:attributes]).to have_key(:recipe_link)
+      expect(user_favorites[0][:attributes]).to have_key(:recipe_title)
+      expect(user_favorites[0][:attributes]).to have_key(:created_at)
+      expect(user_favorites[0][:attributes]).to_not have_key(:user_api_key)
+
+      expect(user_favorites[0][:type]).to eq("favorite")
+      expect(user_favorites[0][:attributes][:country]).to eq("Ireland")
+      expect(user_favorites[0][:attributes][:recipe_link]).to eq("http://www.edamam.com/recipe/cheddar-colcannon-torte-97c47f3162d83a9da30673929bbf8f7a/ireland")
+      expect(user_favorites[0][:attributes][:recipe_title]).to eq("Cheddar Colcannon Torte")        
+    end 
+
+    it "sad path: returns a 400 error and message that user could not be found if an invalid 
+      user_api_key passed in" do
+        get "/api/v1/favorites?user_api_key=123456abcd"
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response).to have_key(:errors)
+        expect(parsed_response[:errors]).to eq("User could not be found")
+    end 
+
+    it "sad path: returns a 400 error and message that user could not be found if no user_api_key passed in" do
+        get "/api/v1/favorites?user_api_key="
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response).to have_key(:errors)
+        expect(parsed_response[:errors]).to eq("User could not be found")
+    end 
   end
-end
+end 
